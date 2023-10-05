78128b641f916c4d59201e6383efa142909b67b0
echugay <alyonachugay@gmail.com>
2023-10-05 16:57:49 -0400

    red side auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/PPBotAutoRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/PPBotAutoRed.java
@@ -6,8 +6,8 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.teamcode.botconfigs.PPBot;
 import org.firstinspires.ftc.teamcode.vision.AprilTagProcessorPosition;
 
-@Autonomous(name="PPBotAuto", group = "ppbot")
-public class PPBotAuto extends LinearOpMode {
+@Autonomous(name="PPBotAutoRed", group = "ppbot")
+public class PPBotAutoRed extends LinearOpMode {
 
     public PPBot robot;
     public AprilTagProcessorPosition vision;
@@ -31,31 +31,31 @@ public class PPBotAuto extends LinearOpMode {
         vision.closeAprilTag();
 
         switch (location) {
-            case 0:
+            case 2:
                 robot.autonomousMove(0, 4, 0, robot.slowSpeed, this);
-                robot.autonomousMove(0, 0, Math.PI / 8, robot.slowSpeed, this);
+                robot.autonomousMove(0, 0, -Math.PI / 8, robot.slowSpeed, this);
                 robot.autonomousMove(0, 24, 0, robot.slowSpeed, this);
                 robot.openClaw();
                 robot.autonomousMove(0, -24, 0, robot.slowSpeed, this);
-                robot.autonomousMove(0, 0, -Math.PI / 8, robot.slowSpeed, this);
+                robot.autonomousMove(0, 0, Math.PI / 8, robot.slowSpeed, this);
                 break;
             case 1:
                 robot.autonomousMove(0, 28, 0, robot.slowSpeed, this);
                 robot.openClaw();
                 robot.autonomousMove(0, -24, 0, robot.slowSpeed, this);
                 break;
-            case 2:
+            case 0:
                 robot.autonomousMove(0, 14, 0, robot.slowSpeed, this);
-                robot.autonomousMove(0, 0, -Math.PI / 4, robot.slowSpeed, this);
+                robot.autonomousMove(0, 0, Math.PI / 4, robot.slowSpeed, this);
                 robot.autonomousMove(0, 14, 0, robot.slowSpeed, this);
                 robot.openClaw();
                 robot.autonomousMove(0, -14, 0, robot.slowSpeed, this);
-                robot.autonomousMove(0, 0, Math.PI / 4, robot.slowSpeed, this);
+                robot.autonomousMove(0, 0, -Math.PI / 4, robot.slowSpeed, this);
                 robot.autonomousMove(0, -10, 0, robot.slowSpeed, this);
                 break;
         }
 
-        robot.autonomousMove(-36, 0, 0, robot.midSpeed, this);
-        robot.autonomousMove(3, 0, 0 , robot.slowSpeed, this);
+        robot.autonomousMove(36, 0, 0, robot.midSpeed, this);
+        robot.autonomousMove(-3, 0, 0 , robot.slowSpeed, this);
     }
 }
